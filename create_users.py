0b04911bc13ee8f65faab40a06c6ac5ff4b4a6db
Sajee Mathew <sajee@sajee.net>
2014-06-06 13:24:58 -0400

    Refactored

--- create_users.py
+++ create_users.py
@@ -32,12 +32,24 @@ def create_users():
             exit(1)
 
     # attach policy to group
-    iam.put_group_policy(group, group + "_Policy", policy_json)
+    # security policy: allows access to everything but IAM
+    # if the IAM lab is included in the day, then remove the line "NotAction": "iam:*",
+    policy = '''{
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "NotAction": "iam:*",
+          "Resource": "*"
+        }
+      ]
+    }'''
+    iam.put_group_policy(group, policy_name, policy)
 
 
     # add users to group
-    with open('users.csv', 'rU') as users_file:
-        user_reader = csv.reader(users_file)
+    with open(DATA_FILE_NAME, 'rU') as data_file:
+        user_reader = csv.reader(data_file)
         for row in user_reader:
             user, password = row[0], row[1]
             try:
@@ -56,8 +68,8 @@ def create_users():
 delete group, remove IAM policy and users.
 """
 def delete_users():
-    with open('users.csv', 'rU') as users_file:
-        user_reader = csv.reader(users_file)
+    with open(DATA_FILE_NAME, 'rU') as data_file:
+        user_reader = csv.reader(data_file)
         for row in user_reader:
             user = row[0]
             try:
@@ -69,32 +81,21 @@ def delete_users():
                 print "Problems deleting %s.  Exiting due to error: %s" % (user, str(e.message))
                 exit(1)
     
-    iam.delete_group_policy(group, group + "_Policy")
+    iam.delete_group_policy(group, policy_name)
     iam.delete_group(group)
 
 
+#################################################################################################
 
-
-# security policy: allows access to everything but IAM
-# if the IAM lab is included in the day, then remove the line "NotAction": "iam:*",
-policy_json = '''{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "NotAction": "iam:*",
-      "Resource": "*"
-    }
-  ]
-}'''
-
+DATA_FILE_NAME = 'users.csv'
 group = "ImmersionDay_Students"
+policy_name = group + "_Policy"
+
 iam = boto.iam.connect_to_region('us-east-1')
 
 if len(sys.argv) == 1:
     print "Please specify create or delete."
     exit(1)
-
 
 if len(sys.argv) == 2:
     if sys.argv[1] == "create":
